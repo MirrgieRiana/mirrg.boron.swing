30cb2f9115952b56804a5bcf9a0b10ff02e13417
Mirrgie Riana <tacticsrealize2@gmail.com>
2019-04-29 12:42:54 +0900

    UtilsColor: update

--- src/main/java/mirrg/boron/swing/UtilsColor.java
+++ src/main/java/mirrg/boron/swing/UtilsColor.java
@@ -5,28 +5,28 @@ import java.awt.Color;
 public class UtilsColor
 {
 
-	// Color creator
+	//////////////////////// IO
 
 	/**
-	 * 引数は0以上255以下の整数値に丸められます。
+	 * 各値は小数点以下を四捨五入されたあと、0～255の範囲に丸められて使用されます。
 	 */
-	public static Color createColor(double r, double g, double b, double a)
+	public static int fromDoublesRGBA(double r, double g, double b, double a)
 	{
-		return createColor((int) r, (int) g, (int) b, (int) a);
+		return fromIntegersRGBA((int) Math.round(r), (int) Math.round(g), (int) Math.round(b), (int) Math.round(a));
 	}
 
 	/**
-	 * 引数は0以上255以下の整数値に丸められます。
+	 * 各値は小数点以下を四捨五入されたあと、0～255の範囲に丸められて使用されます。
 	 */
-	public static Color createColor(double r, double g, double b)
+	public static int fromDoublesRGB(double r, double g, double b)
 	{
-		return createColor((int) r, (int) g, (int) b);
+		return fromIntegersRGB((int) Math.round(r), (int) Math.round(g), (int) Math.round(b));
 	}
 
 	/**
-	 * 引数は0以上255以下の整数値に丸められます。
+	 * 各値は0～255の範囲に丸められて使用されます。
 	 */
-	public static Color createColor(int r, int g, int b, int a)
+	public static int fromIntegersRGBA(int r, int g, int b, int a)
 	{
 		if (r < 0) r = 0;
 		if (g < 0) g = 0;
@@ -36,13 +36,13 @@ public class UtilsColor
 		if (g >= 256) g = 255;
 		if (b >= 256) b = 255;
 		if (a >= 256) a = 255;
-		return new Color(r, g, b, a);
+		return (a << 24) | (r << 16) | (g << 8) | b;
 	}
 
 	/**
-	 * 引数は0以上255以下の整数値に丸められます。
+	 * 各値は0～255の範囲に丸められて使用されます。
 	 */
-	public static Color createColor(int r, int g, int b)
+	public static int fromIntegersRGB(int r, int g, int b)
 	{
 		if (r < 0) r = 0;
 		if (g < 0) g = 0;
@@ -50,23 +50,88 @@ public class UtilsColor
 		if (r >= 256) r = 255;
 		if (g >= 256) g = 255;
 		if (b >= 256) b = 255;
-		return new Color(r, g, b);
+		return (r << 16) | (g << 8) | b;
 	}
+
+	//
+
+	public static int fromIntegerArrayRGBA(int[] rgba)
+	{
+		return fromIntegersRGBA(rgba[0], rgba[1], rgba[2], rgba[3]);
+	}
+
+	public static int fromIntegerArrayRGB(int[] rgba)
+	{
+		return fromIntegersRGB(rgba[0], rgba[1], rgba[2]);
+	}
+
+	public static void toIntegerArrayRGBA(int[] destRgba, int argb)
+	{
+		destRgba[3] = (argb >> 24) & 0xff;
+		destRgba[0] = (argb >> 16) & 0xff;
+		destRgba[1] = (argb >> 8) & 0xff;
+		destRgba[2] = (argb >> 0) & 0xff;
+	}
+
+	public static int[] toIntegerArrayRGBA(int argb)
+	{
+		int[] arrayRgba = new int[4];
+		toIntegerArrayRGBA(arrayRgba, argb);
+		return arrayRgba;
+	}
+
+	public static void toIntegerArrayRGB(int[] destRgb, int rgb)
+	{
+		destRgb[0] = (rgb >> 16) & 0xff;
+		destRgb[1] = (rgb >> 8) & 0xff;
+		destRgb[2] = (rgb >> 0) & 0xff;
+	}
+
+	public static int[] toIntegerArrayRGB(int rgb)
+	{
+		int[] arrayRgb = new int[3];
+		toIntegerArrayRGB(arrayRgb, rgb);
+		return arrayRgb;
+	}
+
+	//
+
+	public static int fromColorRGBA(Color colorRgba)
+	{
+		return colorRgba.getRGB();
+	}
+
+	public static int fromColorRGB(Color colorRgb)
+	{
+		return colorRgb.getRGB() & 0xffffff;
+	}
+
+	public static Color toColorRGBA(int argb)
+	{
+		return new Color(argb, true);
+	}
+
+	public static Color toColorRGB(int rgb)
+	{
+		return new Color(rgb, false);
+	}
+
+	//////////////////////// conversion
 
 	// Random Color
 
-	public static Color createColorRandom(int minR, int maxR, int minG, int maxG, int minB, int maxB, int minA, int maxA)
+	public static int randomRGBA(int minR, int maxR, int minG, int maxG, int minB, int maxB, int minA, int maxA)
 	{
-		return createColor(
+		return fromIntegersRGBA(
 			(int) (Math.random() * (maxR - minR + 1) + minR),
 			(int) (Math.random() * (maxG - minG + 1) + minG),
 			(int) (Math.random() * (maxB - minB + 1) + minB),
 			(int) (Math.random() * (maxA - minA + 1) + minA));
 	}
 
-	public static Color createColorRandom(int minR, int maxR, int minG, int maxG, int minB, int maxB)
+	public static int randomRGB(int minR, int maxR, int minG, int maxG, int minB, int maxB)
 	{
-		return createColor(
+		return fromIntegersRGB(
 			(int) (Math.random() * (maxR - minR + 1) + minR),
 			(int) (Math.random() * (maxG - minG + 1) + minG),
 			(int) (Math.random() * (maxB - minB + 1) + minB));
@@ -75,94 +140,41 @@ public class UtilsColor
 	/**
 	 * アルファ成分は不透明で固定です。
 	 */
-	public static Color createColorRandom(int min, int max)
+	public static int randomRGB(int min, int max)
 	{
-		return createColorRandom(min, max, min, max, min, max);
+		return randomRGB(min, max, min, max, min, max);
 	}
 
 	/**
 	 * 完全に不透明で各成分が0以上255以下のランダムな色を返します。
 	 */
-	public static Color createColorRandom()
+	public static int randomRGB()
 	{
-		return createColorRandom(0, 255);
+		return randomRGB(0, 255);
 	}
 
 	/**
 	 * 完全に不透明で各成分が0以上127以下のランダムな色を返します。
 	 */
-	public static Color createColorRandomDark()
+	public static int randomRGBDark()
 	{
-		return createColorRandom(0, 127);
+		return randomRGB(0, 127);
 	}
 
 	/**
 	 * 完全に不透明で各成分が128以上255以下のランダムな色を返します。
 	 */
-	public static Color createColorRandomLight()
+	public static int randomRGBLight()
 	{
-		return createColorRandom(128, 255);
+		return randomRGB(128, 255);
 	}
-
-	// int color
-
-	public static int getARGB(int[] RGBA)
-	{
-		return getARGB(RGBA[0], RGBA[1], RGBA[2], RGBA[3]);
-	}
-
-	public static int getRGB(int[] RGB)
-	{
-		return getRGB(RGB[0], RGB[1], RGB[2]);
-	}
-
-	public static int getARGB(int r, int g, int b, int a)
-	{
-		return (a << 24) | (r << 16) | (g << 8) | b;
-	}
-
-	public static int getRGB(int r, int g, int b)
-	{
-		return (r << 16) | (g << 8) | b;
-	}
-
-	public static void getFromARGB(int[] destRGBA, int argb)
-	{
-		destRGBA[3] = (argb >> 24) & 0xff;
-		destRGBA[0] = (argb >> 16) & 0xff;
-		destRGBA[1] = (argb >> 8) & 0xff;
-		destRGBA[2] = (argb >> 0) & 0xff;
-	}
-
-	public static void getFromRGB(int[] destRGB, int rgb)
-	{
-		destRGB[0] = (rgb >> 16) & 0xff;
-		destRGB[1] = (rgb >> 8) & 0xff;
-		destRGB[2] = (rgb >> 0) & 0xff;
-	}
-
-	public static int[] getFromARGB(int argb)
-	{
-		int[] RGBA = new int[4];
-		getFromARGB(RGBA, argb);
-		return RGBA;
-	}
-
-	public static int[] getFromRGB(int rgb)
-	{
-		int[] RGB = new int[4];
-		getFromRGB(RGB, rgb);
-		return RGB;
-	}
-
-	// Ratio Color
 
 	/**
 	 * 色a, bの中間色を計算します。
 	 * ratioが0以下の場合はaを返し、ratioが1以上の場合はbを返します。
 	 * ratioが小数の場合は、a, bを適切な比率で混合した色を計算して返します。
 	 */
-	public static int getARGBRatio(double ratio, int a, int b)
+	public static int ratio(double ratio, int a, int b)
 	{
 		if (ratio <= 0) return a;
 		if (ratio >= 1) return b;
@@ -170,10 +182,10 @@ public class UtilsColor
 		int[] a2 = new int[4];
 		int[] b2 = new int[4];
 
-		getFromARGB(a2, a);
-		getFromARGB(b2, b);
+		toIntegerArrayRGBA(a2, a);
+		toIntegerArrayRGBA(b2, b);
 
-		return getARGB(
+		return fromIntegersRGBA(
 			(int) ((1 - ratio) * a2[0] + ratio * b2[0]),
 			(int) ((1 - ratio) * a2[1] + ratio * b2[1]),
 			(int) ((1 - ratio) * a2[2] + ratio * b2[2]),
--- src/test/java/mirrg/boron/swing/TestUtilsColor.java
+++ src/test/java/mirrg/boron/swing/TestUtilsColor.java
@@ -13,15 +13,40 @@ public class TestUtilsColor
 	@Test
 	public void test()
 	{
-		assertEquals(new Color(0, 255, 0, 255), createColor(-1, 256, -10000, 10000));
-		assertEquals(new Color(3, 6, 11, 66), createColorRandom(3, 3, 6, 6, 11, 11, 66, 66));
-		assertEquals(new Color(35, 63, 135, 221), new Color(getARGB(getFromARGB(new Color(35, 63, 135, 221).getRGB())), true));
+		assertEquals(0xFFFFFFFF, fromDoublesRGBA(301, 302, 303, 304));
+		assertEquals(0xFFFFFF, fromDoublesRGB(301, 302, 303));
+		assertEquals(0x00000000, fromDoublesRGBA(-1, -2, -3, -4));
+		assertEquals(0x000000, fromDoublesRGB(-1, -2, -3));
+		assertEquals(0x00000000, fromDoublesRGBA(-0.1, -0.2, -0.3, -0.4));
+		assertEquals(0x000000, fromDoublesRGB(-0.1, -0.2, -0.3));
+		assertEquals(0x03000102, fromDoublesRGBA(0.1, 1.1, 2.1, 3.1));
+		assertEquals(0x000102, fromDoublesRGB(0.1, 1.1, 2.1));
+		assertEquals(0x04010203, fromDoublesRGBA(0.5, 1.5, 2.5, 3.5));
+		assertEquals(0x010203, fromDoublesRGB(0.5, 1.5, 2.5));
 
-		assertEquals(new Color(0, 255, 0), createColor(-1, 256, -10000));
-		assertEquals(new Color(3, 6, 11), createColorRandom(3, 3, 6, 6, 11, 11));
-		assertEquals(new Color(35, 63, 135), new Color(getRGB(getFromRGB(new Color(35, 63, 135).getRGB()))));
+		assertEquals(0x04010203, fromIntegerArrayRGBA(new int[] { 1, 2, 3, 4 }));
+		assertEquals(0x010203, fromIntegerArrayRGB(new int[] { 1, 2, 3, 4 }));
+		assertArrayEquals(new int[] { 1, 2, 3, 4 }, toIntegerArrayRGBA(0x04010203));
+		assertArrayEquals(new int[] { 1, 2, 3 }, toIntegerArrayRGB(0x010203));
 
-		assertEquals(new Color(40, 60, 80, 100).getRGB(), getARGBRatio(0.25, new Color(20, 40, 60, 80).getRGB(), new Color(100, 120, 140, 160).getRGB()));
+		assertEquals(0x04010203, fromColorRGBA(new Color(1, 2, 3, 4)));
+		assertEquals(0x010203, fromColorRGB(new Color(1, 2, 3)));
+		assertEquals(new Color(1, 2, 3, 4), toColorRGBA(0x04010203));
+		assertEquals(new Color(1, 2, 3), toColorRGB(0x010203));
+
+		assertEquals(0x04010203, randomRGBA(1, 1, 2, 2, 3, 3, 4, 4));
+		assertEquals(0x010203, randomRGB(1, 1, 2, 2, 3, 3));
+
+		assertEquals(0x10203040, ratio(-99999, 0x10203040, 0x50607080));
+		assertEquals(0x10203040, ratio(-1, 0x10203040, 0x50607080));
+		assertEquals(0x10203040, ratio(-0.0001, 0x10203040, 0x50607080));
+		assertEquals(0x10203040, ratio(0, 0x10203040, 0x50607080));
+		assertEquals(0x20304050, ratio(0.25, 0x10203040, 0x50607080));
+		assertEquals(0x30405060, ratio(0.5, 0x10203040, 0x50607080));
+		assertEquals(0x40506070, ratio(0.75, 0x10203040, 0x50607080));
+		assertEquals(0x50607080, ratio(1, 0x10203040, 0x50607080));
+		assertEquals(0x50607080, ratio(1.00001, 0x10203040, 0x50607080));
+		assertEquals(0x50607080, ratio(99999, 0x10203040, 0x50607080));
 	}
 
 }
